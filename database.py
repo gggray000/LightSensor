0d2b912af06b085fc35188e240328b4559345e94
ganruilin <ruilin.gan@st.oth-regensburg.de>
2024-07-04 18:54:00 +0200

    changed datetime()

--- database.py
+++ database.py
@@ -7,7 +7,7 @@ def init_db():
         CREATE TABLE IF NOT EXISTS light (
             id INTEGER PRIMARY KEY AUTOINCREMENT,
             light_detected INTEGER NOT NULL,
-            timestamp DATE DEFAULT (datetime('now','localtime'))
+            timestamp DATE DEFAULT (datetime('now'))
         )
     ''')
     conn.commit()
