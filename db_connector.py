7f041b0ac69bee4db78ab411fe7e9dcf57b08712
gggray000 <124888446+gggray000@users.noreply.github.com>
2025-05-05 18:09:16 +0200

    Fixed influxDB write and read logic, and time display problem.

--- db_connector.py
+++ db_connector.py
@@ -1,10 +1,9 @@
 import os
 from datetime import datetime
-
 import pytz
 from influxdb_client import InfluxDBClient, Point, WritePrecision
 
-INFLUXDB_URL = os.environ.get("INFLUXDB_URL", "http://localhost:8086")
+INFLUXDB_URL = os.environ.get("INFLUXDB_URL", "http://influxdb2:8086")
 INFLUXDB_TOKEN = os.environ.get("INFLUXDB_TOKEN")
 INFLUXDB_ORG = os.environ.get("INFLUXDB_ORG", "Dynamics e.V.")
 INFLUXDB_BUCKET = os.environ.get("INFLUXDB_BUCKET", "light_sensor")
@@ -19,20 +18,36 @@ write_api = client.write_api()
 
 def get_time():
     tz = pytz.timezone("Europe/Berlin")
-    time_with_tz = datetime.now(tz).strftime("%Y-%m-%d %H:%M:%S")
+    time_with_tz = datetime.now(tz).isoformat()
     return time_with_tz
 
 def write_to_influx(value):
     point = Point("light_data") \
         .field("value", value) \
-        .time(get_time(), WritePrecision.NS)
+        .time(get_time())
     write_api.write(bucket=INFLUXDB_BUCKET, org=INFLUXDB_ORG, record=point)
 
 def get_data():
     query_api = client.query_api()
-    query = 'from(bucket:"light_sensor")\
-    |> range(start: -10m)\
-    |> filter(fn:(r) => r._measurement == "light_data") \
-    |> limit (n:1)'
+    query = '''
+        from(bucket: "light_sensor")
+        |> range(start: -10m)
+        |> filter(fn: (r) => 
+            r._measurement == "light_data" and 
+            r._field == "value"
+        )
+        |> last()
+        '''
     result = query_api.query(org=INFLUXDB_ORG, query=query)
-    return result
+
+    for table in result:
+        for record in table.records:
+            return {
+                "value": record.get_value(),
+                "time": record.get_time()
+            }
+
+    return {
+        "value": None,
+        "time": None
+    }
--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-from flask import Flask, request, render_template
+import logging
+
+import pytz
+from flask import Flask, request, render_template, jsonify
 from flask_cors import CORS
 from db_connector import write_to_influx, get_data
 
@@ -9,15 +12,27 @@ CORS(app)
 def add_brightness():
     # bright == 0, dark == 1
     light_detected = request.json['light_detected']
-    write_to_influx(light_detected)
+    try:
+        write_to_influx(light_detected)
+    except Exception as e:
+        return jsonify({"error": str(e)})
+    return jsonify({"message": "Data added successfully"}), 200
 
 @app.route('/', methods=['GET'])
 def show_data():
     record = get_data()
-    data = [{
-        'light_detected': record["value"],
-        'timestamp': record["time"],
-    }]
+    print(f"Retrieved record from InfluxDB: {record}")
+    if record["value"] is None or record["time"] is None:
+        data = [{
+            'light_detected': 'No data',
+            'timestamp': 'N/A',
+        }]
+    else:
+        data = [{
+            'light_detected': record["value"],
+            'timestamp': record["time"].astimezone(pytz.timezone("Europe/Berlin")).strftime("%Y-%m-%d %H:%M:%S"),
+        }]
+    print(f"Data: {data}")
     return render_template('showData.html', data=data)
 
 if __name__ == '__main__':
